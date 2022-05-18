2c990c6f7528fb626f1ba1cd05017807e9d65e62
ortheog17@gmail.com <ortheog17@gmail.com>
2022-05-18 11:28:11 +0300

    Initial-Commit

---
+++ API_ROUTEHW/Controllers/BooksController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_ROUTEHW.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [Route("{id:min(5)}")]
+        public string GetById()
+        {
+            return "Hello From Id min 5";
+        }
+
+        [Route("{id:alpha}")]
+        public string GetByAlphaId()
+        {
+            return "Hello From Alpha id , Dont know to do the ragex ... ";
+        }
+    }
+}
